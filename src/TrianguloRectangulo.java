d6f4dd74fde5fb84c895f0f4255b0084ad2fe095
ManuelMonteNaranco <3lp3r4s04@gmail.com>
2024-01-13 16:04:34 +0100

    Progra9

---
+++ src/TrianguloRectangulo.java
@@ -0,0 +1,28 @@
+public class TrianguloRectangulo {
+    private double cateto1;
+    private double cateto2;
+
+    public TrianguloRectangulo(double cateto1, double cateto2){
+        this.cateto1 = cateto1;
+        this.cateto1 = cateto1;
+    }
+
+    public double getCateto1() {
+        return cateto1;
+    }
+
+    public double getCateto2() {
+        return cateto2;
+    }
+
+    public void setCateto1(double cateto1) {
+        this.cateto1 = cateto1;
+    }
+
+    public void setCateto2(double cateto2) {
+        this.cateto2 = cateto2;
+    }
+    public double obtenerHipotenusa(){
+        return Math.sqrt(Math.pow(cateto1,2)+Math.pow(cateto2,2)); //raices cuadradas
+    }
+}
